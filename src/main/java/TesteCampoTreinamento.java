479771d6911cdc67c349767bccb1c5068ea74a6f
henriquebulla <31052264+henriquebulla@users.noreply.github.com>
2020-11-03 19:40:06 -0300

    Teste EditText

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -14,8 +14,22 @@ public class TesteCampoTreinamento {
 	public void testeTextField() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("file:///" +  System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		driver.findElement(By.xpath("//*[@id=\"elementosForm:nome\"]")).sendKeys("HenriqueBulla");
-		assertEquals("HenriqueBulla", driver.findElement(By.xpath("//*[@id=\"elementosForm:nome\"]")).getAttribute("value"));
+		driver.findElement(By.xpath("//*[@id=\"elementosForm:nome\"]")).sendKeys("Henrique");
+		assertEquals("Henrique", driver.findElement(By.xpath("//*[@id=\"elementosForm:nome\"]")).getAttribute("value"));
+		
+		driver.findElement(By.xpath("//*[@id=\"elementosForm:sobrenome\"]")).sendKeys("Rezende Bulla");
+		assertEquals("Rezende Bulla", driver.findElement(By.xpath("//*[@id=\"elementosForm:sobrenome\"]")).getAttribute("value"));
+		
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void testeTextArea() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" +  System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.xpath("//*[@id=\"elementosForm:sugestoes\"]")).sendKeys("HenriqueBulla");
+		assertEquals("Rezende Bulla", driver.findElement(By.xpath("//*[@id=\"elementosForm:sugestoes\"]")).getAttribute("value"));
 		driver.quit();
 		
 	}
